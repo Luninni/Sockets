06c493a26676b6b93a72eec92c265d00d487cef9
Luninni <valentina.alarcon@ug.uchile.cl>
2020-05-13 21:09:33 -0400

    Create cliente.py

---
+++ cliente.py
@@ -0,0 +1,19 @@
+import sys
+import socket
+
+client = socket.socket()
+host = 'localhost'
+port = 8889
+
+# Connecting
+client.connect((host,port))
+#print ('Connected to ...')
+
+message = 'How much gold do you have?'
+while True:
+    client.send(message.encode('ascii'))
+
+    data = client.rcv(1024)
+    print('Recibido:', str(data.decode('ascii')))
+
+    ans = input('')
